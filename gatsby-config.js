9521005940437ea22b8c50a9e747ae9eda3b782c
Kaleb Alves <kaleblucasalves@hotmail.com>
2023-03-01 20:07:11 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Site de teste 3`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
